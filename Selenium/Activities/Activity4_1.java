4c5969af137ab871e0f8ac39dcca9a61c6332390
VidhyaNair92 <110611686+VidhyaNair92@users.noreply.github.com>
2022-08-16 22:38:56 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity4_1.java
@@ -0,0 +1,18 @@
+package activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity4_1 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.training-support.net");
+        String title = driver.getTitle();
+        System.out.println("Title of the page is: " +title);
+        driver.findElement(By.xpath("//a[text()='About Us']")).click();
+        String newPageTitle = driver.getTitle();
+        System.out.println("Title of the page is: " +newPageTitle);
+    }
+}
